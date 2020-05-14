74a7d5641168f11a80ee001664019d8cca4eb5a5
Fuyutsuki <glacierluna@gmail.com>
2020-05-14 18:44:10 +0800

    feat: 实现图层拖拽功能

--- src/components/layers/TextLayer.vue
+++ src/components/layers/TextLayer.vue
@@ -2,6 +2,7 @@
   <div class="text-layer">
     <div
       class="content"
+      ref="content"
       :style="layerStyle"
       contenteditable="true"
       @click.capture="$emit('click-layer')"
@@ -40,6 +41,28 @@ export default {
     isSelected() {
       return this.layerData === this.curLayer;
     }
+  },
+  methods: {
+    handleDragLayer(e) {
+      if (e.target !== this.$refs.content) {
+        return;
+      }
+      let x0 = e.clientX;
+      let y0 = e.clientY;
+      const handleMove = ({ clientX, clientY, target }) => {
+        this.layerData.x = this.layerData.x + clientX - x0;
+        this.layerData.y = this.layerData.y + clientY - y0;
+        x0 = clientX;
+        y0 = clientY;
+      };
+      document.addEventListener("mousemove", handleMove);
+      document.addEventListener("mouseup", () => {
+        document.removeEventListener("mousemove", handleMove);
+      });
+    }
+  },
+  mounted() {
+    document.addEventListener("mousedown", this.handleDragLayer);
   }
 };
 </script>
