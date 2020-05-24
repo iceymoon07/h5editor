cc350b0fbe86fc0e894a01b0965039d649259780
Fuyutsuki <glacierluna@gmail.com>
2020-05-24 21:26:26 +0800

    fix: 图片 url

--- src/layer/ImageLayer.js
+++ src/layer/ImageLayer.js
@@ -2,7 +2,7 @@ import BaseLayer from './BaseLayer';
 import { LAYER_TYPES } from './common';
 
 export default class ImageLayer extends BaseLayer {
-    constructor(url = 'http://localhost:8081/public/img/demo.jpg', x, y, w, h) {
+    constructor(url = 'https://iceymoon07.github.io/h5editor/public/img/demo.jpg', x, y, w, h) {
         super(x, y, w, h, LAYER_TYPES.IMAGE);
         this.url = url;
     }
